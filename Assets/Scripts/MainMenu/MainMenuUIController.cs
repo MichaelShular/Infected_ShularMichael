81120bcbb136d0dfafc6a3ff83350610f5b98952
Mick <mickx.shular@gmail.com>
2022-04-21 21:11:12 -0400

    UI fixes and instructions

--- Assets/Scripts/MainMenu/MainMenuUIController.cs
+++ Assets/Scripts/MainMenu/MainMenuUIController.cs
@@ -5,13 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUIController : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject instructionUI;
+
     public void OnStartGamePressed()
     {
         SceneManager.LoadScene("Game");
     }
 
+
     public void OnQuitPressed()
     {
         Application.Quit();
+    }
+
+
+    public void ToggleInsturtcions()
+    {
+        instructionUI.SetActive(!instructionUI.activeSelf);
     }
 }
